a04eb01b5976298493e96ad919d01c2f1566cc57
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-03-06 15:36:18 +0100

    Implementing the Outer Facing Contract (Part 2)

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 namespace CourseLibrary.API.Controllers
 {
     [ApiController]
+    [Route("api/authors")]
+    /*[Route("api/[controller]")] */// is the same like above
     public class AuthorsController : ControllerBase 
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
@@ -19,6 +21,7 @@ namespace CourseLibrary.API.Controllers
                 throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
 
+        [HttpGet()]
         public IActionResult GetAuthors()
         {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
