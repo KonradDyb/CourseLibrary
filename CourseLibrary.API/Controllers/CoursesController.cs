ed37fb70559817a1b6b7f6457eb414080569780f
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-03-12 19:35:11 +0100

    Creating a Child Resource

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -38,7 +38,7 @@ namespace CourseLibrary.API.Controllers
             return Ok(_mapper.Map<IEnumerable<CourseDto>>(coursesForAuthorFromRepo));
         }
         
-        [HttpGet("{courseId}")]
+        [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
         public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
         {
             if (!_courseLibraryRepository.AuthorExists(authorId))
@@ -55,5 +55,25 @@ namespace CourseLibrary.API.Controllers
 
             return Ok(_mapper.Map<CourseDto>(courseForAuthorFromRepo));
         }
-    }
+
+        [HttpPost]
+        public ActionResult<CourseDto> CreateCourseForAuthor(
+            Guid authorId, CourseForCreationDto course)
+        {
+            //  that means that URI to resource doesn't exists
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseEntity = _mapper.Map<Entities.Course>(course);
+            _courseLibraryRepository.AddCourse(authorId, courseEntity);
+            _courseLibraryRepository.Save();
+
+            var courseToReturn = _mapper.Map<CourseDto>(courseEntity);
+            return CreatedAtRoute("GetCourseForAuthor",
+                new { authorId = courseToReturn.AuthorId, courseId = courseToReturn.Id },
+                courseToReturn);
+        }
+    } 
 }
