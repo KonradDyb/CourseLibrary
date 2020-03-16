fadc48e786612b5de0a9af054b4ee81360fa6747
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-03-16 18:16:02 +0100

    Class-level Input Validation with a Custom Attribute

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto : IValidatableObject
+    // Custom attributes are executed before the validate method gets called,
+    // and that can come in handy for property-level validation.
+    // Yet at class level, the same rules still apply.
+    // If property-level validation fails, class-level validation
+    // will not occur even when using custom attributes.
+    [CourseTitleMustBeDifferentFromDescription]
+    public class CourseForCreationDto /*: IValidatableObject*/
     {
         [Required]
         [MaxLength(100)]
@@ -11,14 +18,14 @@ namespace CourseLibrary.API.Models
         [MaxLength(1500)]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title == Description)
-            {
-                yield return new ValidationResult(
-                    "The provided description should be different from the title.",
-                    new[] { "CourseForCreationDto" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided description should be different from the title.",
+        //            new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
