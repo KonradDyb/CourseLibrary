34ce6e42a36be63f50dfb249b411812f155b947b
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-03-16 17:48:20 +0100

    Validating Input with Data Annotations

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
     public class CourseForCreationDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(1500)]
         public string Description { get; set; }
     }
 }
