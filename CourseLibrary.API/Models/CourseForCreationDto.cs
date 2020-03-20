1d6fe525793ab506ffc155db0a6dd0055ef23217
Konrad Dybkowski <kdybkowski1@gmail.com>
2020-03-20 13:22:56 +0100

    Validating Input when Updating a Resource with PUT

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -4,29 +4,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
-    // Custom attributes are executed before the validate method gets called,
-    // and that can come in handy for property-level validation.
-    // Yet at class level, the same rules still apply.
-    // If property-level validation fails, class-level validation
-    // will not occur even when using custom attributes.
-    [CourseTitleMustBeDifferentFromDescription(
-        ErrorMessage = "Title must be different from description")]
-    public class CourseForCreationDto /*: IValidatableObject*/
-    {
-        [Required(ErrorMessage = "You should fill out a title.")]
-        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
-        public string Title { get; set; }
-        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
-        public string Description { get; set; }
 
-        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        //{
-        //    if (Title == Description)
-        //    {
-        //        yield return new ValidationResult(
-        //            "The provided description should be different from the title.",
-        //            new[] { "CourseForCreationDto" });
-        //    }
-        //}
+    public class CourseForCreationDto : CourseForManipulationDto
+    {
+        
     }
 }
